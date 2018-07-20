924f4f9828136d82858256df89062e022881d8b0
elzup <guild0105@gmail.com>
2018-07-20 23:07:56 +0900

    Generators

--- src/index.js
+++ src/index.js
@@ -1,30 +1,54 @@
 // @flow
 
-type Rule = {
+type RuleDiv = {|
   n: number,
   label: string,
-  // priority: number,
+|}
+
+type RuleComp = {|
+  check: (v: number) => boolean,
+  label: string,
+|}
+
+type Rule = RuleDiv | RuleComp
+
+function convertComp(rule: Rule): RuleComp {
+  if (rule.check) {
+    return rule
+  }
+  const { n } = rule // @HACKME
+  return {
+    label: rule.label,
+    check: v => v % n === 0,
+  }
 }
 
 class FizzBuzz {
   limit: number
-  rules: Rule[]
+  rules: RuleComp[]
 
   constructor(limit: number = 1000, rules: Rule[] = []) {
     this.limit = limit
-    this.rules = rules
+    this.rules = rules.map(convertComp)
   }
 
   *it(n: number = this.limit): Generator<string, void, string> {
-    let i = 0
-    while (i < n) {
-      yield '1'
+    let i = 1
+    const hit = (r: RuleComp) => r.check(i)
+    while (i <= n) {
+      const hitRules = this.rules.filter(hit)
+      if (hitRules.length === 0) {
+        yield `${i}`
+      } else {
+        yield hitRules.map(v => v.label).join('')
+      }
       i++
     }
   }
 
   take(n: number = this.limit): string[] {
-    return []
+    const it = this.it(n)
+    return [...it]
   }
 }
 
--- src/test.js
+++ src/test.js
@@ -3,10 +3,15 @@ const M = require('.').default
 
 test('snapshot', () => {
   const m = new M()
-  expect(m.take()).toMatchSnapshot()
+  expect(m.take(3)).toMatchSnapshot('to 3')
+  expect(m.take(10)).toMatchSnapshot('to 10')
 
   const t = m.it()
-  expect(t.next().value).toMatchSnapshot()
-  expect(t.next().value).toMatchSnapshot()
-  expect(t.next().value).toMatchSnapshot()
+  expect([
+    t.next().value,
+    t.next().value,
+    t.next().value,
+    t.next().value,
+    t.next().value,
+  ]).toMatchSnapshot('iterator')
 })
