58625c2c0eccf47f4bc3912de1b782ff6b4d4583
elzup <guild0105@gmail.com>
2018-08-02 13:38:53 +0900

    Change to Inline snapshot

--- src/test.js
+++ src/test.js
@@ -3,44 +3,157 @@ const fizzbuzz = require('.').default
 
 test('take', () => {
   const fb = fizzbuzz()
-  expect(fb.take()).toMatchSnapshot('to 30')
-  expect(fb.take(3)).toMatchSnapshot('to 3')
-  expect(fb.take(10, 5)).toMatchSnapshot('to 10 from 5')
+  expect(fb.take(15)).toMatchInlineSnapshot(`
+Array [
+  "1",
+  "2",
+  "Fizz",
+  "4",
+  "Buzz",
+  "Fizz",
+  "7",
+  "8",
+  "Fizz",
+  "Buzz",
+  "11",
+  "Fizz",
+  "13",
+  "14",
+  "FizzBuzz",
+]
+`)
+  expect(fb.take(10, 5)).toMatchInlineSnapshot(`
+Array [
+  "Buzz",
+  "Fizz",
+  "7",
+  "8",
+  "Fizz",
+  "Buzz",
+]
+`)
 })
 
 test('chain', () => {
   const fb = fizzbuzz(10)
-  expect(fb.to(8).take()).toMatchSnapshot('1~8')
-  expect(fb.from(3).take()).toMatchSnapshot('3~10')
+  expect(fb.to(5).take()).toMatchInlineSnapshot(
+    `
+Array [
+  "1",
+  "2",
+  "Fizz",
+  "4",
+  "Buzz",
+]
+`
+  )
+  expect(fb.from(3).take()).toMatchInlineSnapshot(
+    `
+Array [
+  "Fizz",
+  "4",
+  "Buzz",
+  "Fizz",
+  "7",
+  "8",
+  "Fizz",
+  "Buzz",
+]
+`
+  )
   expect(
     fb
       .from(3)
       .to(8)
       .take()
-  ).toMatchSnapshot('3~8')
-  expect(fb.rules([{ name: 'test', n: 3 }]).take(5)).toMatchSnapshot('rule')
+  ).toMatchInlineSnapshot(
+    `
+Array [
+  "Fizz",
+  "4",
+  "Buzz",
+  "Fizz",
+  "7",
+  "8",
+]
+`
+  )
+  expect(fb.rules([{ name: 'test', n: 3 }]).take(5)).toMatchInlineSnapshot(
+    `
+Array [
+  "1",
+  "2",
+  "test",
+  "4",
+  "5",
+]
+`
+  )
 })
 
 test('at', () => {
   const fb = fizzbuzz()
-  expect(fb.at(3)).toMatchSnapshot('3 fizz')
-  expect(fb.at(5)).toMatchSnapshot('5 buzz')
-  expect(fb.at(15)).toMatchSnapshot('15 fizzbuzz')
-  expect(fb.at(7)).toMatchSnapshot('7')
+  expect(fb.at(7)).toMatchInlineSnapshot(`"7"`)
+  expect(fb.at(3)).toMatchInlineSnapshot(`"Fizz"`)
+  expect(fb.at(5)).toMatchInlineSnapshot(`"Buzz"`)
+  expect(fb.at(15)).toMatchInlineSnapshot(`"FizzBuzz"`)
 })
 
 test('argument', () => {
-  expect(fizzbuzz().take(5)).toMatchSnapshot('empty')
-  expect(fizzbuzz(5).take()).toMatchSnapshot('to')
-  expect(fizzbuzz(5).take(2)).toMatchSnapshot('overwrite number')
-  expect(fizzbuzz({ to: 3, from: 2, rules: [] }).take()).toMatchSnapshot(
-    'froms'
+  expect(fizzbuzz().take(5)).toMatchInlineSnapshot(
+    `
+Array [
+  "1",
+  "2",
+  "Fizz",
+  "4",
+  "Buzz",
+]
+`
+  )
+  expect(fizzbuzz(5).take()).toMatchInlineSnapshot(
+    `
+Array [
+  "1",
+  "2",
+  "Fizz",
+  "4",
+  "Buzz",
+]
+`
+  )
+  expect(fizzbuzz(5).take(2)).toMatchInlineSnapshot(
+    `
+Array [
+  "1",
+  "2",
+]
+`
+  )
+  expect(fizzbuzz({ to: 3, from: 2, rules: [] }).take()).toMatchInlineSnapshot(
+    `
+Array [
+  "2",
+  "3",
+]
+`
   )
 })
 
 test('addRule', () => {
   const fb = fizzbuzz(10).addRule({ name: 'hey', n: 2 })
-  expect(fb.take(6)).toMatchSnapshot('with hey')
+  expect(fb.take(6)).toMatchInlineSnapshot(
+    `
+Array [
+  "1",
+  "hey",
+  "Fizz",
+  "hey",
+  "Buzz",
+  "Fizzhey",
+]
+`
+  )
 })
 
 test('custom func', () => {
@@ -50,7 +163,22 @@ test('custom func', () => {
       { name: '_toshino_', check: v => v === 5 },
     ],
   })
-  expect(fb.take(10)).toMatchSnapshot('to 3')
+  expect(fb.take(10)).toMatchInlineSnapshot(
+    `
+Array [
+  "1",
+  "KYOKO",
+  "3",
+  "KYOKO",
+  "_toshino_",
+  "KYOKO",
+  "7",
+  "KYOKO",
+  "9",
+  "KYOKO",
+]
+`
+  )
 })
 
 test('it', () => {
@@ -62,5 +190,15 @@ test('it', () => {
     t.next().value,
     t.next().value,
     t.next().value,
-  ]).toMatchSnapshot('works')
+  ]).toMatchInlineSnapshot(
+    `
+Array [
+  "1",
+  "2",
+  "Fizz",
+  "4",
+  "Buzz",
+]
+`
+  )
 })
